dd55168b45a8644bdca59547e24209298f56fb64
Miles Pomeroy <miles@byu.net>
2011-01-24 22:19:03 -0700

    Finish first draft of database.

--- database.sql
+++ database.sql
@@ -101,18 +101,102 @@ CREATE INDEX stocks_source_id_idx ON stocks(source_id);
 CREATE INDEX stocks_viability_id_idx ON stocks(viability_id);
 CREATE INDEX stocks_group_id_idx ON stocks(group_id);
 
+CREATE TABLE comments (
+  id          SERIAL,
+  stock_id    INTEGER,
+  comment     TEXT,
+  created_by  INTEGER,
+  created_ts  TIMESTAMP WITH TIME ZONE,
+  CONSTRAINT comments_pk PRIMARY KEY (id),
+  CONSTRAINT comments_stock_id_fk 
+    FOREIGN KEY (stock_id) REFERENCES stocks(id),
+  CONSTRAINT comments_created_by_fk
+    FOREIGN KEY (created_by) REFERENCES users(id)
+);
+COMMENT ON TABLE comments IS 'Comments by group users on individual stocks.';
+COMMENT ON COLUMN comments.created_by IS 'FK: User id that commented.';
+CREATE INDEX comments_stock_id_idx ON comments(stock_id);
+
+CREATE TABLE citations (
+  id        SERIAL,
+  stock_id  INTEGER,
+  entry     TEXT,
+  url       TEXT,
+  CONSTRAINT citations_pk PRIMARY KEY (id),
+  CONSTRAINT citations_stock_id_fk
+    FOREIGN KEY (stock_id) REFERENCES citations(id)
+);
+COMMENT ON TABLE citations IS 'Published work that references a stock.';
+COMMENT ON COLUMN citations.entry IS 'Text of the formatted citation.';
+COMMENT ON COLUMN citations.url IS 'Optional web address.';
+CREATE INDEX citations_stock_id_idx ON citations(stock_id);
+
+CREATE TABLE chromosomes (
+  id          SERIAL,
+  species_id  INTEGER NOT NULL,
+  name        VARCHAR(2) NOT NULL,
+  CONSTRAINT chromosomes_pk PRIMARY KEY (id),
+  CONSTRAINT chromosomes_species_id_fk
+    FOREIGN KEY (species_id) REFERENCES species(id)
+);
+COMMENT ON TABLE chromosomes IS 'Chromosomes for each species.';
+COMMENT ON COLUMN chromosomes.name IS 'Could be letter or number.';
+
+CREATE TABLE affected_chromosomes ( -- AffectedChromosome model
+  stock_id      INTEGER,
+  chromosome_id INTEGER,
+  CONSTRAINT affected_chromosomes_pk PRIMARY KEY (stock_id, chromosome_id),
+  CONSTRAINT affected_chromosomes_stock_id_fk
+    FOREIGN KEY (stock_id) REFERENCES stocks(id),
+  CONSTRAINT affected_chromosomes_chromosome_id_fk
+    FOREIGN KEY (chromosome_id) REFERENCES chromosomes(id)
+);
+COMMENT ON TABLE affected_chromosomes IS 'Chromosomes affected in mutation of stock. Many-to-many.';
+
+
 -- Insert default values
 INSERT INTO roles (id, name) VALUES (1, 'App Administrator');
 INSERT INTO roles (id, name) VALUES (2, 'Group Administrator');
 INSERT INTO roles (id, name) VALUES (3, 'User');
 INSERT INTO roles (id, name) VALUES (4, 'Inactive User');
 
-INSERT INTO species (id, name, short_name) 
-VALUES (1, 'Drosophila Melanogaster', 'Fly');
+INSERT INTO species (
+  id, 
+  name, 
+  short_name
+) 
+VALUES (
+  1, 
+  'Drosophila Melanogaster', 
+  'Fly'
+);
 
 INSERT INTO viabilities (id, name) VALUES (1, 'Viable');
 INSERT INTO viabilities (id, name) VALUES (2, 'Lethal');
 INSERT INTO viabilities (id, name) VALUES (3, 'Homozygous Lethal');
 
+INSERT INTO chromosomes (species_id, name) VALUES (1, 'X');
+INSERT INTO chromosomes (species_id, name) VALUES (1, 'Y');
+INSERT INTO chromosomes (species_id, name) VALUES (1, '2');
+INSERT INTO chromosomes (species_id, name) VALUES (1, '3');
+INSERT INTO chromosomes (species_id, name) VALUES (1, '4');
+
+
 -- Insert test values
 INSERT INTO groups (name) VALUES ('Marsh Lab');
+INSERT INTO users (
+  username, 
+  name, 
+  password,
+  group_id, 
+  role_id
+)
+VALUES (
+  'miles', 
+  'Miles Pomeroy', 
+  'test',
+  (SELECT id FROM groups WHERE name = 'Marsh Lab'),
+  1 -- App Admin
+);
+
+
